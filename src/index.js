a6f75dd4566913ef6af907738dfcb03fc4b0eea7
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-21 19:25:23 +0100

    API Integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  let recipeSpace = document.querySelector("#recipe");
+  let instructionsInput = document.querySelector("#instructions");
 
-  new Typewriter("#recipe", {
-    strings: "The Recipe Will Go Here",
-    autoStart: true,
-    delay: 50,
-  });
+  let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
+  let prompt = `Unser Instructions = Generate a Spanish Recipe Idea which uses ${instructionsInput.value} as its main ingredient`;
+  let context =
+    "You are an experienced Spanish chef. Please give a recipe idea which includes the ingredient provided by the user. It should be only the name of the dish and a short description";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let formElement = document.querySelector("#recipeForm");
