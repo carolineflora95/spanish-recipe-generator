f98225ecb44ca63c310a60c69aa6fc115524b8fb
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-21 19:45:03 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#instructions");
 
   let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
-  let prompt = `Unser Instructions = Generate a Spanish Recipe Idea which uses ${instructionsInput.value} as its main ingredient`;
+  let prompt = `User Instructions = Generate a Spanish Recipe Idea which uses ${instructionsInput.value} as its main ingredient`;
   let context =
-    "You are an experienced Spanish chef. Please give a recipe idea which includes the ingredient provided by the user. It should be only the name of the dish and a short description";
+    "You are an experienced Spanish chef. Please give a recipe idea which includes the ingredient provided by the user. It should be only the name of the dish and a short description, but don't label the sections";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating"> ⏲️ Generating a Spanish Recipe Using ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
